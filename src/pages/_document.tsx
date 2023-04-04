8569182c56991ac219a38cdc795f76b9c890b1fa
Dmitriy Izotov <disaythis@gmail.com>
2023-04-04 08:21:44 +0300

    fix: изменил custom document шаблон

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,22 +1,7 @@
 import Document, { Html, Head, Main, NextScript, DocumentContext, DocumentInitialProps } from 'next/document';
-
-export default function MyfDocument(): JSX.Element {
-	return (
-		<Html lang="ru">
-			<Head />
-			<body>
-				<Main />
-				<NextScript />
-			</body>
-		</Html>
-	);
-}
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 class MyDocument extends Document {
 	static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
 		const initialProps = await Document.getInitialProps(ctx);
-
 		return initialProps;
 	}
 
@@ -32,3 +17,5 @@ class MyDocument extends Document {
 		);
 	}
 }
+
+export default MyDocument;
